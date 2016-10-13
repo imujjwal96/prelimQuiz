773e43d8eea1338fe5722ef5e20080dbedf5890a
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2016-10-13 23:57:26 +0530

    Update RegisterModel Doc

--- application/models/RegisterModel.php
+++ application/models/RegisterModel.php
@@ -1,7 +1,20 @@
 <?php
 
+/**
+ * Class RegisterModel
+ * Handles user registration
+ */
 class RegisterModel {
 
+    /**
+     * Registers a user
+     * @param string $name. User's name
+     * @param string $email. User's email
+     * @param string $userName. User's username
+     * @param string $phone. User's phone number
+     * @param string $role. User's role (admin / contestant)
+     * @return bool true if user registered successfully, else false
+     */
     public static function registerNewUser($name, $email, $userName,  $phone, $role = 'contestant') {
 
         $database = DatabaseFactory::getFactory()->getConnection();
@@ -23,14 +36,25 @@ class RegisterModel {
         return false;
     }
 
+    /**
+     * Validates form inputs
+     * @param string $userName. User's username
+     * @param string $email. User's email
+     * @return bool true if credentials are validated, else false
+     */
     public static function formValidation($userName, $email) {
         if (self::validateUserName($userName) AND self::validateEmail($email)) {
             return true;
         }
-
         return false;
     }
 
+    /**
+     * Validates a user's username
+     * @param string $userName. User's username
+     * @return bool true if username is validated, else false
+     * Username should contain only alphanumeric characters of length in between 2 and 64
+     */
     public static function validateUserName($userName) {
         if (empty($userName)) {
             return false;
@@ -43,6 +67,11 @@ class RegisterModel {
         return true;
     }
 
+    /**
+     * Validates a user's email
+     * @param string $email. User's email
+     * @return bool true if email is validated, else false
+     */
     public static function validateEmail($email) {
         if (empty($email)) {
             return false;
@@ -51,7 +80,6 @@ class RegisterModel {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             return false;
         }
-
         return true;
     }
 
