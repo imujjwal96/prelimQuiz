e4c5c34bef47e20e099e1a237e3c206a940f161d
Liam Norman <liamworkza@gmail.com>
2017-09-29 11:23:56 +0200

    Deny access to install.php after installation (#48)
    * delete installer.php after installing quiz site
    
    * added check for if config file is generated, stop execution of install.php

--- public/install.php
+++ public/install.php
@@ -1,5 +1,9 @@
 <?php
 
+if (file_exists ("../Application/Config/config.php")) {
+    die('Installation already completed...');
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["quiz_name"], $_POST["quiz_type"], $_POST["db_name"], $_POST["db_type"],
         $_POST["db_user"], $_POST["db_pass"], $_POST["db_port"])) {
